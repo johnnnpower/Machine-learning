71c33a8c27638c6f2ef7e7d6bff994bd986cc437
johnnnpower <155130755+johnnnpower@users.noreply.github.com>
2023-12-28 23:49:16 -0500

    Create detect_iris.py

---
+++ detect_iris.py
@@ -0,0 +1,32 @@
+# Importer la bibliothèque scikit-learn
+import sklearn
+
+# Charger l'ensemble de données iris
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+# Afficher les noms des caractéristiques et des cibles
+print(iris.feature_names)
+print(iris.target_names)
+
+# Créer les variables X et y qui contiennent les caractéristiques et les cibles
+X = iris.data
+y = iris.target
+
+# Diviser les données en ensemble d'entraînement et de test
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Créer le modèle k-NN avec k=3
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=3)
+
+# Entraîner le modèle sur les données d'entraînement
+knn.fit(X_train, y_train)
+
+# Prédire les cibles sur les données de test
+y_pred = knn.predict(X_test)
+
+# Évaluer la précision du modèle
+from sklearn.metrics import accuracy_score
+print("La précision du modèle est de :", accuracy_score(y_test, y_pred))
